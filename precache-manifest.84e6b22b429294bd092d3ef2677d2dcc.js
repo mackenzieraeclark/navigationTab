8e47703e6d5c25b2a0d147ef70f6586e58efc4bf
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-12-07 18:07:39 -0500

    Updates

---
+++ precache-manifest.84e6b22b429294bd092d3ef2677d2dcc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1e2f74999905d1cd549aef9139e3f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98af74743f1c0d9b2d49",
+    "url": "/static/css/4.63bfcaf7.chunk.css"
+  },
+  {
+    "revision": "31dc3435b51a71f83fae",
+    "url": "/static/css/main.d8778d95.chunk.css"
+  },
+  {
+    "revision": "98af74743f1c0d9b2d49",
+    "url": "/static/js/4.ec2cb732.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/4.ec2cb732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38bffa846c1685d4917",
+    "url": "/static/js/5.82fc762d.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/5.82fc762d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b624fa300622a74b9b",
+    "url": "/static/js/6.3fd61458.chunk.js"
+  },
+  {
+    "revision": "31dc3435b51a71f83fae",
+    "url": "/static/js/main.abbd3f79.chunk.js"
+  },
+  {
+    "revision": "1a52084bbaa37d73e5d3",
+    "url": "/static/js/runtime-main.df8f77be.js"
+  },
+  {
+    "revision": "9cbe7d28fe40a40dfaeb",
+    "url": "/static/js/stencil-h8k-logo_2-entry-js.a67d665f.chunk.js"
+  },
+  {
+    "revision": "e7a491b799244ef77880",
+    "url": "/static/js/stencil-polyfills-css-shim.0806bdcf.chunk.js"
+  }
+]);
